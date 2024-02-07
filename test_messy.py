4db575f853aa119c8e6d6a0df2b02b0077ef4edb
Faruk D. <faruk.diblen@resoftlabs.com>
2024-02-07 13:22:32 +0100

    updat the test script

--- test_messy.py
+++ test_messy.py
@@ -12,17 +12,20 @@ if __name__ == "__main__":
         port=10022
     )
 
-    # # submit a new job
-    # messy.submit_job()
-
-    # # download files from the server
-    # messy.get_files(
-    #     remote_folder="/home/xenon/filesystem-test-fixture",
-    #     local_folder="./copy_test"
-    # )
+    # submit a new job
+    job_id, stdin, stdout, stderr = messy.submit_job(
+        job_file="/home/xenon/test-slurm.job"
+    )
+    print(f'job_id {job_id}')
 
     # copy files to the server
     messy.put_files(
         remote_folder="/home/xenon/",
         local_folder="./test_folder"
+    )
+
+    # download files from the server
+    messy.get_files(
+        remote_folder="/home/xenon/test_folder",
+        local_folder="./copy_of_test_folder"
     )
